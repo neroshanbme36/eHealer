71fea1df58b73e5c0d560d5d12351cc44a3b04b4
Mayura <mayuraselvarajah26@gmail.com>
2021-04-04 22:41:17 +0530

    header component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,22 +3,26 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     // If component should be shared you need declare and export
     // Importantly dont declare in app.module.ts if you are going to share
     // You cant import component if u are connected with mainRepo
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
+    IonicModule,
     RouterModule
   ],
   exports: [
     CommonModule,
     FormsModule,
-    IonicModule
+    IonicModule,
+    HeaderComponent
   ]
 })
 
