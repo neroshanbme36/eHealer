39f4f7a5afa52c4784c1cf3675004dc2a91f76cb
Mayura <mayuraselvarajah26@gmail.com>
2021-04-04 22:45:00 +0530

    account module

---
+++ client/src/app/account/account.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AccountPageRoutingModule } from './account-routing.module';
+
+import { AccountPage } from './account.page';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    AccountPageRoutingModule
+  ],
+  declarations: [
+    AccountPage,
+    LoginComponent,
+    RegisterComponent
+  ]
+})
+export class AccountPageModule {}
