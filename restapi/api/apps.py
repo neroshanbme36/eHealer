e308b48233e89861b0589eba53ab8241ca7e3be8
Neroshan <Neroshan@visualbusinessretail.com>
2021-04-22 10:33:23 +0530

    scheduler

--- restapi/api/apps.py
+++ restapi/api/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        print('Starting scheduler...')
+        from .scheduler_tasks import session_updater
+        session_updater.start()
